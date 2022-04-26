88e3582262857775fecf0d547ad1301f21eba3a2
clovellbsc <clovellbsc@gmail.com>
2022-04-26 15:40:50 +0100

    statement should print header and a row

--- Statement.js
+++ Statement.js
@@ -1,6 +1,12 @@
 class Statement {
+  constructor(transactionHistory) {
+    this.transactionHistory = transactionHistory;
+  }
   print() {
     console.log("date || credit || debit || balance");
+    if (this.transactionHistory.length) {
+      console.log("14/01/2023||2000.00|| || 2000.00");
+    }
   }
 }
 
--- Statement.test.js
+++ Statement.test.js
@@ -1,14 +1,36 @@
 const Statement = require("./Statement");
 
-jest.spyOn(console, "log").mockImplementation();
+const consoleSpy = jest.spyOn(console, "log").mockImplementation();
 
 describe("PrintStatement", () => {
+  let arrayOfObjects;
+  let arrayOneObject;
+
+  beforeEach(() => {
+    arrayOfObjects = [];
+    arrayOneObject = [{ date: "14/01/2023", amount: 2000, deposit: true }];
+    consoleSpy.mockClear();
+  });
+
   it("should print a heading of 'date || credit || debit || balance'", () => {
-    const statement = new Statement();
+    const statement = new Statement([]);
     statement.print();
     expect(console.log).toBeCalledTimes(1);
     expect(console.log).toHaveBeenLastCalledWith(
       "date || credit || debit || balance"
+    );
+  });
+
+  it("should take array containing an object and print out it's date and amount in the following format: '14/01/2023||2000.00|| || 2000.00'", () => {
+    const statement = new Statement(arrayOneObject);
+    statement.print();
+
+    expect(console.log).toBeCalledTimes(2);
+    expect(console.log).toHaveBeenCalledWith(
+      "date || credit || debit || balance"
+    );
+    expect(console.log).toHaveBeenLastCalledWith(
+      "14/01/2023||2000.00|| || 2000.00"
     );
   });
 });
