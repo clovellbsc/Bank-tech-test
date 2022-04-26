78de6edb1e677a60ab9d4338b0f8aecdb8b5924a
clovellbsc <clovellbsc@gmail.com>
2022-04-26 21:26:52 +0100

    feature test created

---
+++ featureTest.test.js
@@ -0,0 +1,27 @@
+const BankAccount = require("./bankAccount");
+
+const consoleSpy = jest.spyOn(console, "log").mockImplementation();
+
+describe("BankAccount feature test", () => {
+  let bank;
+  beforeEach(() => {
+    consoleSpy.mockClear();
+    bank = new BankAccount();
+  });
+
+  it("should print the same value as expected in the client's requirements", () => {
+    bank.deposit(1000);
+    bank.deposit(2000);
+    bank.withdraw(500);
+    bank.printStatement();
+    date = new Date().toLocaleDateString();
+
+    expect(console.log).toHaveBeenCalledWith(`${date} || || 500.00 || 2500.00`);
+    expect(console.log).toHaveBeenCalledWith(
+      `${date} || 2000.00 || || 3000.00`
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      `${date} || 1000.00 || || 1000.00`
+    );
+  });
+});
