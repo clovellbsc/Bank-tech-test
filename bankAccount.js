6f12978b86eec6843247e6e00bf83ba258866911
clovellbsc <clovellbsc@gmail.com>
2022-04-28 12:00:49 +0100

    Dependency injection of the Transaction class added

--- bankAccount.js
+++ bankAccount.js
@@ -2,19 +2,20 @@ const Transaction = require("./transaction");
 const Statement = require("./Statement");
 
 class BankAccount {
-  constructor(statementClass = Statement) {
+  constructor(statementClass = Statement, transactionClass = Transaction) {
     this.transactionHistory = [];
     this.statementClass = statementClass;
+    this.transactionClass = transactionClass;
   }
 
   deposit(amount) {
-    const deposit = new Transaction(amount);
+    const deposit = new this.transactionClass(amount);
     deposit.deposit();
     this.transactionHistory.push(deposit);
   }
 
   withdraw(amount) {
-    const withdrawal = new Transaction(amount);
+    const withdrawal = new this.transactionClass(amount);
     this.transactionHistory.push(withdrawal);
   }
 
